fa9a89c73323cedf2c0e5bf5bd47befd1ca8d38e
Naoki Hidaka <naoki.hidaka@dena.com>
2023-02-22 12:50:41 +0900

    環境変数によって扱うMaterial Designのバージョンを切り替えられるようにした

---
+++ lib/material2_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Material2App extends StatelessWidget {
+  const Material2App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
---
+++ lib/material3_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Material3App extends StatelessWidget {
+  const Material3App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
